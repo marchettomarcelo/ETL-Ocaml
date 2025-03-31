3f61b94e8b1e3b0aa5ff7397de9f756d603a7985
marchettomarcelo <marcelovfm@al.insper.edu.br>
2025-03-31 14:41:37 -0300

    logic fix

--- bin/main.ml
+++ bin/main.ml
@@ -1,30 +1,21 @@
 open Etl
 
-
 let () =
-
   Db_utils.create_table ();
 
-  if Array.length Sys.argv < 2 then failwith "Please provide an order origin:";
+  if Array.length Sys.argv < 3 then failwith "Please provide an order status and origin:";
 
   let status = Sys.argv.(1) in
   let origin = Sys.argv.(2) in
 
   let order_item = 
-
     Fetch_csv.fetch_csv_from_url "https://raw.githubusercontent.com/marchettomarcelo/order-data/refs/heads/main/order_item.csv"
-    
     |> List.map Utils.row_to_order_item
-  
   in
 
   Fetch_csv.fetch_csv_from_url "https://raw.githubusercontent.com/marchettomarcelo/order-data/refs/heads/main/order.csv"
-
   |> List.map Utils.row_to_order
-  
-  |> (if status = "all" && origin = "all" then fun x -> x
-      else List.filter (fun (order : Utils.order) -> order.status = status && order.origin = origin))
-
+  |> List.filter (fun (order : Utils.order) -> (status = "all" || order.status = status) && (origin = "all" || order.origin = origin))
   |> List.map (fun (order : Utils.order) ->
          [
            string_of_int order.id;
@@ -32,7 +23,3 @@ let () =
            string_of_float (Utils.sum_by_order_id (fun item -> item.tax_paid) order.id order_item);
          ])
   |> Db_utils.save_csv_to_sqlite
-
-
-  
-  
